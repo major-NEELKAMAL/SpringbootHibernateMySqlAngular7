a885fce199c01b26f43158548fbaf333cf03b79e
major-NEELKAMAL <addressneel@gmail.com>
2018-11-27 22:05:51 +0530

    removed crossorigin from repository

--- src/main/java/com/neelkamal/dao/CustomerDAO.java
+++ src/main/java/com/neelkamal/dao/CustomerDAO.java
@@ -7,14 +7,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.neelkamal.model.Customer;
 
-@RepositoryRestResource
-@CrossOrigin(origins = "http://localhost:4200")
+
 public interface CustomerDAO extends JpaRepository<Customer, Integer>{
 	
 	@Query("from Customer where address like ?1%")	
